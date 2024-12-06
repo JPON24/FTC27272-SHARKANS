b0df944524b373a95fabbc67717927051c248580
JPON24 <jpon24@shaw.ca>
2024-12-05 22:19:37 -0700

    comments
    added comments for understanding, no housekeeping required

--- Extension_1.java
+++ Extension_1.java
@@ -5,31 +5,36 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.robot.Robot;
 
 public class Extension_1 {
-    final double deadZone = 0.3;
-    final double speed = 1;
+    final double deadZone = 0.3; //amount of controller input needed to register input
+    final double speed = 1; //speed at which it extends/retracts
     
-    private DcMotor extension_1 = null;
-    
+    private DcMotor extension_1 = null;//reference to object
+
+    //called from starter both and auton, makes reference to control hub to get dcmotor data and configures for use
     public void init(HardwareMap hwMap)
     {
         extension_1 = hwMap.get(DcMotor.class, "extension_1");
-        extension_1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        extension_1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        extension_1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);//ensures that the position is intially 0
+        extension_1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);//means that it will run solely off of set power, no target pos
     }
     //range: 0 to -508
     //min distance to rotate: -225
     public void move(double targetPower)
     {
+        // if pushing up on stick
         if (targetPower > deadZone)
         {
+            //extend the arm (lower speed)
             extension_1.setPower(speed*0.4); 
         }
-        else if(targetPower< -deadZone)
+        else if(targetPower< -deadZone) // if pushing down on stick
         {
+            //retract the arm
             extension_1.setPower(-speed);
         }
-        else
+        else //if not pushing stick
         {
+            // do not move
             extension_1.setPower(0);
         }
     }
