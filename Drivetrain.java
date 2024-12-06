3368b071b8291b386405b6ceea0864067b10ffe1
JPON24 <jpon24@shaw.ca>
2024-12-05 22:07:37 -0700

    field orientation and housekeeping
    added some functionality for field orientation
    // issue //
    the motors will not go at max speed whilst using field orientation
    (theoretically)
    
    added some comments for clarity
    removed unused code and added clutter

--- Drivetrain.java
+++ Drivetrain.java
@@ -1,178 +1,85 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.robot.Robot;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 public class Drivetrain{
-    final int mmFtConversion = 305; 
-    final float vectorPadding = 0.2F;
+    private double speedScalar = 1; // used to control speed with bumpers
     
-    double rpm = 312;
-    double gearRatio = 1;
-    double gearReduction = 19.1;
-    double wheelCircumference = 76.2 * 3.14;
-    double countsPerGearRev = rpm * gearRatio;
-    double countsPerDegree = countsPerGearRev/360;
-    double countsPerWheelRev = countsPerGearRev * gearReduction;
-    double countsPerMM = countsPerWheelRev/wheelCircumference;
-    
-    static double initialPosition;
-    static boolean calibrating = false;
-    static public int actionId = 0;
-    
-    private double speedScalar = 1;
+    //motor references
     private DcMotor frontLeft = null;
     private DcMotor frontRight = null;
     private DcMotor backLeft = null;
     private DcMotor backRight = null;
+
+    IMU imu;
     
     public void init(HardwareMap hwMap)
     {
+        // intialize motors
         frontLeft = hwMap.get(DcMotor.class, "FrontLeft");
         frontRight = hwMap.get(DcMotor.class, "FrontRight");
         backLeft = hwMap.get(DcMotor.class, "BackLeft");
         backRight = hwMap.get(DcMotor.class, "BackRight");
-        
+
+        //intialize imu
+        imu = hwMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot.LogoFacingDirection logoDir = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDir = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot robotOrientationInit = new RevHubOrientationOnRobot(logoDir,usbDir);
+        imu.initialize(new IMU.Parameters(robotOrientationInit));
+        imu.resetYaw();
+
+        //set their run modes to run without encoder, meaning they will not be using setTargetPos
         frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
-    void Calibrate(double targetPosition)
-    {
-        calibrating = true;
-        initialPosition = frontLeft.getCurrentPosition();
-        frontLeft.setTargetPosition((int)(targetPosition * countsPerMM));
-        backLeft.setTargetPosition((int)(targetPosition * countsPerMM));
-        frontRight.setTargetPosition((int)(targetPosition * countsPerMM));
-        backRight.setTargetPosition((int)(targetPosition * countsPerMM));
-    }
-        
-    public void targetPositionTranslate(int targetPowerX, int targetPowerY, int rotation, double targetPosition, int id)
-    {
-        // left: positive
-        // down: positive
-        // right: negative
-        // up: negative
-        if(!calibrating)
-        {
-            Calibrate(targetPosition);
-        }
-        if(id!=actionId) {return;}
-        switch(targetPowerX)
-        {
-            case 1:
-                if (frontLeft.getCurrentPosition() < initialPosition - targetPosition * countsPerMM)
-                {
-                    translate(1,0,0);
-                }
-                else
-                {
-                    translate(0,0,0);
-                    calibrating = false;
-                    actionId++;
-                }
-                break;
-            case -1:
-                if (frontLeft.getCurrentPosition() > initialPosition + targetPosition * countsPerMM)
-                {
-                    translate(-1,0,0);
-                }
-                else
-                {
-                    translate(0,0,0);
-                    calibrating = false;
-                    actionId++;
-                }
-                break;
-            default:
-                break;
-        }    
-        switch(targetPowerY)
-        {
-            case 1:
-                if (frontLeft.getCurrentPosition() < initialPosition - targetPosition * countsPerMM)
-                {
-                    translate(0,-1,0);
-                }
-                else
-                {
-                    translate(0,0,0);
-                    calibrating = false;
-                    actionId++;
-                }
-                break;
-            case -1:
-                if (frontLeft.getCurrentPosition() > initialPosition + targetPosition * countsPerMM)
-                {
-                    translate(0,1,0);
-                }
-                else
-                {
-                    translate(0,0,0);
-                    calibrating = false;
-                    actionId++;
-                }
-               break;
-            default:
-               break;
-        }
-        switch(rotation)
-        {
-            case 1:
-                if (frontLeft.getCurrentPosition() - countsPerDegree < initialPosition - targetPosition * countsPerDegree)
-                {
-                    translate(0,0,1);
-                }
-                else
-                {
-                    translate(0,0,0);
-                    calibrating = false;
-                    actionId++;
-                }
-                break;
-            case -1:
-                if (frontLeft.getCurrentPosition() + countsPerDegree > initialPosition + targetPosition * countsPerDegree)
-                {
-                    translate(0,0,-1);
-                }
-                else
-                {
-                    translate(0,0,0);
-                    calibrating = false;
-                    actionId++;
-                }
-                break;
-            default:
-                break;
-        }
-        /*
-        double frontLeftPower = -rotation + targetPowerY -targetPowerX;
-        double frontRightPower = -rotation -targetPowerY -targetPowerX;
-        double backLeftPower = -rotation + targetPowerY + targetPowerX;
-        double backRightPower = -rotation -targetPowerY + targetPowerX;
-        
-        frontLeft.setPower(frontLeftPower);
-        frontRight.setPower(frontRightPower);
-        backLeft.setPower(backLeftPower);
-        backRight.setPower(backRightPower);*/
-    }
 
+    public void fieldOrientedTranslate(double targetPowerX, double targetPowerY, double rotation)
+    {
+        //get rotation from imu
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        double yaw = orientation.getYaw(AngleUnit.DEGREES);
+
+        double polarControllerInput = Math.atan2(targetPowerY,targetPowerX) * 180/PI; // get controller stick dir
+
+        double theta = (180 - polarControllerInput) + yaw; //convert to -180<=x<=180 and add offset
+
+        //scale based on theta
+        double flScalar = sin(theta);
+        double frScalar = cos(theta);
+        double blScalar = cos(theta);
+        double brScalar = sin(theta);
+
+        //apply scalars + basic code
+        double frontLeftPower = (-rotation + targetPowerY -targetPowerX) * flScalar;
+        double frontRightPower = (-rotation -targetPowerY -targetPowerX) * frScalar;
+        double backLeftPower = (-rotation + targetPowerY + targetPowerX) * blScalar;
+        double backRightPower = (-rotation -targetPowerY + targetPowerX) * brScalar;
+
+        //set speed * speed scalar
+        frontLeft.setPower(frontLeftPower * speedScalar);
+        frontRight.setPower(frontRightPower * speedScalar);
+        backLeft.setPower(backLeftPower * speedScalar);
+        backRight.setPower(backRightPower * speedScalar);
+    }
     
     public void translate(double targetPowerX, double targetPowerY, double rotation)
     {
-        /*frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
-        backLeft.setDirection(DcMotor.Direction.FORWARD);
-        backRight.setDirection(DcMotor.Direction.FORWARD);*/
-        
+        // inputs target power on x and y, outputs proper power distribution
         double frontLeftPower = -rotation + targetPowerY -targetPowerX;
         double frontRightPower = -rotation -targetPowerY -targetPowerX;
         double backLeftPower = -rotation + targetPowerY + targetPowerX;
         double backRightPower = -rotation -targetPowerY + targetPowerX;
-        
+
+        //sets power of each motor
         frontLeft.setPower(frontLeftPower * speedScalar);
         frontRight.setPower(frontRightPower * speedScalar);
         backLeft.setPower(backLeftPower * speedScalar);
@@ -181,6 +88,7 @@ public class Drivetrain{
     
     public void SetSpeedScalar(double change)
     {
+        //sets the speed scalar of the motors to a specified value in starter bot
         speedScalar = change;
     }
 }
