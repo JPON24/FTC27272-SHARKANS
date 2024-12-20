a4cb6c31dc0c4c8b3bcd133b17464a1fb7d04bb2
JPON24 <jpon24@shaw.ca>
2024-12-20 15:54:18 -0700

    Update Sensor1.java

--- Sensor1.java
+++ Sensor1.java
@@ -6,6 +6,17 @@ import com.qualcomm.hardware.maxbotix.MaxSonarI2CXL;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.teamcode.Drivetrain;
+
+import org.firstinspires.ftc.robotcore.external.*;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import com.qualcomm.robotcore.robot.Robot;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import com.qualcomm.hardware.sparkfun.*;
 //import com.qualcomm.robotcore.util.ElapsedTime;
 
 
@@ -14,9 +25,14 @@ public class Sensor1 {
     ActionHandler action = new ActionHandler();
     //ElapsedTime runtime = new ElapsedTime();
     
+    IMU imu;
     MaxSonarI2CXL leftSensor = null;
     MaxSonarI2CXL backSensor = null;
     MaxSonarI2CXL rightSensor = null;
+
+    SparkFunOTOS odometry;
+    Pose2D pos;
+    double imuReading = 0;
     
     boolean calibrating = false;
     
@@ -26,12 +42,80 @@ public class Sensor1 {
     
     public void init(HardwareMap hwMap)
     {
+        imu = hwMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot.LogoFacingDirection logoDir = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDir = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot robotOrientationInit = new RevHubOrientationOnRobot(logoDir,usbDir);
+        imu.initialize(new IMU.Parameters(robotOrientationInit));
+        imu.resetYaw();
+
         leftSensor = hwMap.get(MaxSonarI2CXL.class, "Sensor1");
         backSensor = hwMap.get(MaxSonarI2CXL.class, "Sensor2");
         rightSensor = hwMap.get(MaxSonarI2CXL.class, "Sensor3");
+        odometry.begin();
+        odometry.calibrateIMU();
+        odometry.setLinearUnit(DistanceUnit.INCH);
+        odometry.setAngularUnit(AngleUnit.DEGREE);
         dt.init(hwMap);
     }
     
+    public void OdometryControl(double speed, double tgtX,double tgtY,double tgtRot)
+    {
+        if (!odometry.isConnected()) {return;}
+
+        Orientation o;
+        o = imu.getRobotOrientation(AxesReference.INTRINSIC,AxesOrder.ZYX,AngleUnit.DEGREES);
+        double yaw = o.firstAngle;
+        yaw = 360-yaw;
+        
+        double rot = 0;
+
+        pos = odometry.getPosition();
+        
+        Pose2D tgtPos = new Pose2D(tgtX,tgtY);
+
+        Pose2D moveVector;
+
+        if (tgtPos.x - pos.x > 0)
+        {
+            moveVector.x = speed;
+        }
+        else if (tgtPos.x - pos.x < 0)
+        {
+            moveVector.x = -speed;
+        }
+        else
+        {
+            moveVector.x = 0;
+        }
+
+        if (tgtPos.y - pos.y > 0)
+        {
+            moveVector.y = speed;
+        }
+        else if (tgtPos.y - pos.y < 0)
+        {
+            moveVector.y = -speed;
+        }
+        else
+        {
+            moveVector.y = 0;
+        }
+
+        if (yaw > tgt - lenience && yaw < tgt + lenience)
+        {
+
+        }
+
+        dt.fieldOrientedTranslate(moveVector.x,moveVector.y);
+    }
+
+    public double getImuReading()
+    {
+        imuReading = odometry.getAngularUnit();
+        return imuReading;
+    }
+
     public void Calibrate()
     {
         calibrating = true;
@@ -40,17 +124,6 @@ public class Sensor1 {
         initialDistanceR = rightSensor.getDistanceSync(DistanceUnit.INCH);
     }
     
-    /*private void ElapsedCalls()
-    {
-        double seconds = runtime.seconds();
-        if (seconds >= 14.5 && seconds <= 15.5)
-        {
-            
-        }
-        
-    }*/
-    
-    //dir: 0 = up; speed = up; 2 = down; 3 = right;
     public void moveToPositionR(double targetPosition,double speed, char dir,int id)
     {
         if(!calibrating)
