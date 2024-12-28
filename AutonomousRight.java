4ff5aa87cc8f6dcef1c3446be104f92bc40d5a4a
Elizabeth6432 <edythe952@stemia.ca>
2024-12-28 16:14:23 -0700

    odometer autonomous right

--- AutonomousRight.java
+++ AutonomousRight.java
@@ -1,20 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.hardware.maxbotix.MaxSonarI2CXL;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.robot.Robot;
-
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import com.qualcomm.robotcore.hardware.IMU;
-//import org.firstinspires.ftc.robotcore.external.navigation;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-
 import org.firstinspires.ftc.teamcode.Drivetrain;
 import org.firstinspires.ftc.teamcode.ClawServo;
 import org.firstinspires.ftc.teamcode.Extension_1;
@@ -29,68 +18,59 @@ public class AutonomousRight extends LinearOpMode
     ClawServo cs = new ClawServo();
     ArmLiftMotor am = new ArmLiftMotor();
     Extension_1 e1 = new Extension_1();
-    Sensor1 s1 = new Sensor1(); 
-    ActionHandler action = new ActionHandler();
-    
-    MaxSonarI2CXL leftSensor = null;
-    MaxSonarI2CXL backSensor = null;
-    MaxSonarI2CXL rightSensor = null;
-    
-    IMU imu;
+    Sensor1 s1 = new Sensor1();
 
     double collisionDetectionRadius = 6.5;
-    public boolean moving = true;
+    boolean moving = true;
     boolean grabbing = true;
-    
-    int id = -1;
-
-    DcMotor extension;
+    int id =0;
     
     private void ContinueMovement()
     {
         moving = true;
     }
-    private void MoveForward(double speed, double rot)
+    
+    private void MoveToPosition(double speed, double tgtX, double tgtY, double rot)
     {
-        double rotationRequired = GetRequiredCorrection(rot);
-        dt.fieldOrientedTranslate(0,speed,rotationRequired * speed);
+        //if (action.GetActionId()!=id){return;}
+        s1.OdometryControl(speed,tgtX,tgtY,rot);
+    }
+    private void MoveForward(double speed)
+    {
+        dt.translate(0,-speed,0);
         UpdateClaw();
     }
-    private void MoveBackward(double speed,double rot)
+    private void MoveBackward(double speed)
     {
-        double rotationRequired = GetRequiredCorrection(rot);
-        dt.fieldOrientedTranslate(0,-speed,rotationRequired * speed);
+        dt.translate(0,speed,0);
         UpdateClaw();
     }
     
-    private void MoveLeft(double speed, double rot)
+    private void MoveLeft(double speed)
     {
-        double rotationRequired = GetRequiredCorrection(rot);
-        dt.fieldOrientedTranslate(-speed,0,rotationRequired * speed);
+        dt.translate(-speed,0,0);
         UpdateClaw();
     }
-    private void MoveRight(double speed, double rot)
+    private void MoveRight(double speed)
     {
-        double rotationRequired = GetRequiredCorrection(rot);
-        dt.fieldOrientedTranslate(speed,0,rotationRequired * speed);
+        dt.translate(speed,0,0);
         UpdateClaw();  
     }
     
     private void Stop()
     {
-        s1.Calibrate();
-        dt.fieldOrientedTranslate(0,0,0);
+        dt.translate(0,0,0);
         UpdateClaw();
     }
     
-    private void LeftRotate(double speed)
+    private void LeftRotate()
     {
-        dt.fieldOrientedTranslate(0,0,-speed);
-        UpdateClaw();
+        dt.translate(0,0,-1);
+         UpdateClaw();
     }
-    private void RightRotate(double speed)
+    private void RightRotate()
     {
-        dt.fieldOrientedTranslate(0,0,speed);
+        dt.translate(0,0,1);
         UpdateClaw();
     }
     
@@ -124,7 +104,6 @@ public class AutonomousRight extends LinearOpMode
     {
         UpdateClaw();
         id++;
-        //if(action.GetActionId()!=id){return;}
         am.rotate(rotation,"A",id);
     }
     
@@ -142,164 +121,45 @@ public class AutonomousRight extends LinearOpMode
         cs.update();
     }
     
-    private void MoveToPosition(double tgt,double spd,char dir)
-    {
-        id++;
-        //if (action.GetActionId()!=id){return;}
-        s1.moveToPositionR(tgt,spd,dir,id);
-    }
-
-    private double GetRequiredCorrection(double tgt)
-    {
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        double yaw = orientation.getYaw(AngleUnit.DEGREES);
-
-        int lenience = 1; //lenience in degrees
-        if (yaw > tgt - lenience && yaw < tgt + lenience)
-        {
-            return 0;
-        }
-        else if (yaw < tgt - lenience)
-        {
-            return -0.05; //turn speed
-        }
-        else if (yaw > tgt + lenience)
-        {
-            return 0.05; //turn speed
-        }
-        return 0;
-    }
-
-    private void IMU_RotationControl(int tgt, double speed, String dir)
-    {
-        id++;
-        //if (id != action.GetActionId()) {return;}
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        double yaw = orientation.getYaw(AngleUnit.DEGREES);
-        double lenience = 3;
-        if (dir == "R")
-        {
-            if (!(yaw > tgt - lenience && yaw < tgt + lenience))
-            {
-                RightRotate(speed);
-            }
-            else
-            {
-                Stop();
-            }
-        }
-        else if (dir == "L")
-        {
-            if (!(yaw > tgt - lenience && yaw < tgt + lenience))
-            {
-                LeftRotate(speed);
-            }
-            else
-            {
-                Stop();
-            }
-        }
-    }
-    
     private void CommandSequence()
     {
-        ActivateGrab(); //grab sample
-        CloseClaw(); //grab sample
-        sleep(700);
-        Extend(1,150);
-        sleep(1000);
-        RotateArm(90);//lift sample
-        Extend(1, 750);//try to reach high chamber
-        sleep(2500);
-        //MoveForward(0.55,0);//go to submersible
-        MoveToPosition(20,0.4,'F');
-        sleep(525);
-        Stop();//reset wheels   
-        sleep(1000);
-        RotateArm(70);//start to clip sample
-        sleep(400);
-        RotateArm(60);//pull 
-        sleep(1000);
-        RotateArm(50);//pull
-        sleep(500);
-        Retract(1,450);//secure latch
-        sleep(1500);
-        OpenClaw();//release specimen
-        DeactivateGrab();//release specimen
-        sleep(500);
-        Stop();
-        sleep(100);
-        
-        //MoveBackward(0.5,0);//prepare to get samples\
-        MoveToPosition(4,0.28,'B');
-        sleep(880);
-        Stop();//reset wheels
-        sleep(400);
-        Retract(1,150);
-        RotateArm(30);//reset arm 
-        ActivateGrab();
-        OpenClaw();
-        //MoveRight(0.5,0);//move to samples
-        /*MoveToPosition(28,0.5,'R');
-        sleep(3000);
-        Stop();
-        sleep(400);*/
-        sleep(1000);
-        //second specimen
-        DeactivateGrab();
-        RotateArm(-15);
-        IMU_RotationControl(-80,0.15,"R");
-        sleep(3000);
-        Stop();
-        sleep(100);
-        MoveToPosition(4,0.5,'R');
-        sleep(400);
-        Stop();
-        sleep(200);
-        MoveToPosition(2,0.1,'R');
-        sleep(400);
-        Stop();
-        sleep(50);
-        ActivateGrab();
-        CloseClaw();
-        sleep(600);
-        Stop();
-        sleep(50);
-        MoveToPosition(6,0.5,'F');
-        sleep(450);
-        Stop();
-        sleep(100);
-        IMU_RotationControl(0,0.25,"L");
-        sleep(1500);
-        RotateArm(30);
-        Extend(1,750);
-        MoveToPosition(20,0.5,'L');
+        MoveToPosition(0.8,0,30.75,0); // 1
+        sleep(1600);
+        MoveToPosition(0.8,48,3,180); // 2
+        sleep(1600);
+        MoveToPosition(0.8,0,30.75,0); // 3
+        sleep(1600);
+        MoveToPosition(0.8,39,18.75,0); // 4
+        sleep(1600);
+        MoveToPosition(0.8,39,29,0); // 5
+        sleep(1600);
+        MoveToPosition(0.8,39,3,180); // 6
+        sleep(1600);
+        MoveToPosition(0.8,49,29,0); // 7
+        sleep(1600);
+        MoveToPosition(0.8,49,3,180); // 8
+        sleep(1600);
+        MoveToPosition(0.8,55.5,56.75,0); // 9
+        sleep(1600);
+        MoveToPosition(0.8,55.5,3,0); // 10
+        sleep(1600);
+        MoveToPosition(0.8,55.5,15,0); // 11
+        sleep(1600);
+        MoveToPosition(0.8,55.5,3,180); // 12
+        sleep(1600);
+        MoveToPosition(0.8,0,30.75,0); // 13
+        sleep(1600);
+        MoveToPosition(0.8,48,3,180); // 2
+        sleep(1600);
+        MoveToPosition(0.8,0,30.75,0); // 3
+        sleep(1600);
+        MoveToPosition(0.8,48,3,180); // 2
+        sleep(1600);
+        MoveToPosition(0.8,0,30.75,0); // 3
+        sleep(1600);
+        MoveToPosition(0.8,48,3,180); // 2
         sleep(1600);
         Stop();
-        sleep(100);
-        RotateArm(90);
-        Extend(1,750);
-        MoveToPosition(8,0.6,'F');
-        sleep(1000);
-        Stop();
-        sleep(100);
-        Retract(1,600);
-        sleep(500);
-        Stop();
-        RotateArm(70);//start to clip sample
-        sleep(400);
-        RotateArm(60);//pull 
-        sleep(1000);
-        RotateArm(50);//pull
-        sleep(500);
-        Retract(1,450);//secure latch
-        sleep(1500);
-        OpenClaw();//release specimen
-        DeactivateGrab();//release specimen
-        sleep(500);
-        MoveToPosition(14,0.2,'B');
-        sleep(400);
-        moving = false;
     }
  
     @Override
@@ -309,46 +169,14 @@ public class AutonomousRight extends LinearOpMode
         cs.init(hardwareMap);
         am.init(hardwareMap);
         e1.init(hardwareMap);
-        s1.init(hardwareMap);
-        
-        leftSensor = hardwareMap.get(MaxSonarI2CXL.class, "Sensor1");
-        backSensor = hardwareMap.get(MaxSonarI2CXL.class, "Sensor2");
-        rightSensor = hardwareMap.get(MaxSonarI2CXL.class, "Sensor3");
-        
-        imu = hardwareMap.get(IMU.class, "imu");
-        extension = hardwareMap.get(DcMotor.class, "extension_1");
-        
-        RevHubOrientationOnRobot.LogoFacingDirection logoDir = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection usbDir = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-        RevHubOrientationOnRobot robotOrientationInit = new RevHubOrientationOnRobot(logoDir,usbDir);
-        imu.initialize(new IMU.Parameters(robotOrientationInit));
-        imu.resetYaw();
-        
+       
         waitForStart();
         ContinueMovement(); //just in case
         while(opModeIsActive())
         {
             if (moving)
             {
-                //IMU_RotationControl(-90,0.4,"R");
                 CommandSequence();
-                //MoveToPosition(10,0.2,'R');
-                
-                double dL = leftSensor.getDistanceSync(50,DistanceUnit.INCH);
-                double dB = backSensor.getDistanceSync(50,DistanceUnit.INCH);
-                double dR = rightSensor.getDistanceSync(50,DistanceUnit.INCH);
-                
-                telemetry.addData("l", dL);
-                telemetry.addData("b", dB);
-                telemetry.addData("r", dR);
-                telemetry.addData("id",id);
-                
-                YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-                double yaw = orientation.getYaw(AngleUnit.DEGREES);
-                telemetry.addData("yaw",yaw);
-                telemetry.update();
-                /*telemetry.addData("extension pos", extension.getCurrentPosition());
-                telemetry.update()*/
             }
         }
     }
