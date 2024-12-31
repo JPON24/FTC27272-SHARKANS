203b90757ca5038d78e33257998bb04261379e16
JPON24 <jpon24@shaw.ca>
2024-12-30 16:20:59 -0800

    possible fix for oscillation issues

--- Sensor1.java
+++ Sensor1.java
@@ -1,51 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.teamcode.Drivetrain;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import com.qualcomm.robotcore.robot.Robot;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import com.qualcomm.robotcore.hardware.IMU;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
-//import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 public class Sensor1 {
     Drivetrain dt = new Drivetrain();
     ActionHandler action = new ActionHandler();
-    //ElapsedTime runtime = new ElapsedTime();
-    
-    IMU imu;
+    ElapsedTime runtime = new ElapsedTime();
 
     SparkFunOTOS odometry;
     SparkFunOTOS.Pose2D pos;
-    
-    boolean calibrating = false;
-    
-    double initialDistanceL = 0;
-    double initialDistanceB = 0;
-    double initialDistanceR = 0;
 
     boolean[] completedBools = new boolean[3];
-    
+    boolean[] canStartTimers = new boolean[3];
+    double[] initTimes = new double[3];
+    int[] polarities = new int[3];
+
     public void init(HardwareMap hwMap)
     {
-        imu = hwMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot.LogoFacingDirection logoDir = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection usbDir = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-        RevHubOrientationOnRobot robotOrientationInit = new RevHubOrientationOnRobot(logoDir,usbDir);
-        imu.initialize(new IMU.Parameters(robotOrientationInit));
-        imu.resetYaw();
-
+        for (int i = 0; i < 3; i++) {canStartTimers[i] = true;}
         odometry = hwMap.get(SparkFunOTOS.class, "otos");
         odometry.begin();
         odometry.setLinearUnit(DistanceUnit.INCH);
@@ -58,7 +40,6 @@ public class Sensor1 {
         if (!odometry.isConnected()) {return;}
 
         double yaw = GetImuReading();
-        double rot = 0;
         double lenience = 3;
 
         pos = odometry.getPosition();
@@ -66,53 +47,66 @@ public class Sensor1 {
         SparkFunOTOS.Pose2D tgtPos = new SparkFunOTOS.Pose2D();
         tgtPos.x = tgtX;
         tgtPos.y = tgtY;
+        tgtPos.h = tgtRot;
 
         SparkFunOTOS.Pose2D moveVector = new SparkFunOTOS.Pose2D();
         
         double xDiff = tgtPos.x - pos.x;
         double yDiff = tgtPos.y - pos.y;
-        
+        double rotDiff = tgtPos.y - pos.h;
+
         if (Math.abs(xDiff) < 1)
         {
             completedBools[0] = true;
+            SetInitTime(0);
+            PolaritySwap(0);
         }
         else
         {
             completedBools[0] = false;
+            canStartTimers[0] = true;
         }
 
         if (Math.abs(yDiff) < 1)
         {
             completedBools[1] = true;
+            SetInitTime(1);
+            PolaritySwap(1);
         }
         else
         {
             completedBools[1] = false;
+            canStartTimers[1] = true;
         }
 
         double maxXYDiff = Math.max(Math.abs(xDiff), Math.abs(yDiff));
         xDiff/=maxXYDiff;
         yDiff/=maxXYDiff;
         
-        moveVector.x = xDiff;
-        moveVector.y = yDiff;
-        
-        if (yaw < tgtRot - lenience)
+        if (pos.h - lenience < rotDiff)
         {
-            rot = speed;
-            completedBools[2] = true;
-        }
-        else if (yaw > tgtRot + lenience)
-        {
-            rot = -speed;
+            moveVector.h = speed;
             completedBools[2] = false;
+            canStartTimers[2] = true;
+        }
+        else if (pos.h + lenience > rotDiff)
+        {
+            moveVector.h = -speed;
+            completedBools[2] = false;
+            canStartTimers[2] = true;
         }
         else
         {
             completedBools[2] = true;
-        }
+            SetInitTime(2);
+            PolaritySwap(2);
+        }        
+        
+        moveVector.x = xDiff * polarities[0];
+        moveVector.y = yDiff * polarities[1];
+        moveVector.h *= polarities[2];
 
-        dt.fieldOrientedTranslate(moveVector.x,moveVector.y,rot);
+        dt.fieldOrientedTranslate(moveVector.x,moveVector.y,moveVector.h);
     }
 
     public double GetImuReading()
@@ -144,5 +138,26 @@ public class Sensor1 {
             }
         }
         return true;
+    }
+
+    private void SetInitTime(int index)
+    {
+        if (canStartTimers[index])
+        {
+            initTimes[index] = runtime.milliseconds();
+            canStartTimers[index] = false;
+        }
+    }
+
+    private void PolaritySwap(int index)
+    {
+        if (runtime.milliseconds() - initTimes[index] > 0.05)
+        {
+            polarities[index] = 1;
+        }
+        else
+        {
+            polarities[index] = -1;
+        }
     }
 }   
