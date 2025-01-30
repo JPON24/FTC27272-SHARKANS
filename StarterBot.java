f8cfd5d68b9c9eea25527e724570c525aa36519d
Elizabeth6432 <edythe952@stemia.ca>
2025-01-29 20:07:03 -0700

    Update StarterBot.java

--- StarterBot.java
+++ StarterBot.java
@@ -23,47 +23,15 @@ public class StarterBot extends LinearOpMode{
     double speedInterval = 0.4;
     
     boolean canShiftWristType = true;
-    boolean canShiftCustomWristPos = true;
     boolean normalWristType = false;
-    boolean lastXButtonState = false;
     
     boolean canShiftArm = true;
     double currentArmSpeed = 1.0;
     double armSpeedInterval = 0.8;
         
-    boolean hookMacroActivated = false;
-
     @Override
     public void runOpMode()
     {
-        double targetPowerX = 0;
-        double targetPowerY = 0;
-        double targetRotation = 0;
-        
-        boolean leftBumperPressed = false;
-        boolean rightBumperPressed = false;
-        boolean dpadUp = false;
-        boolean aButtonPressed1 = false;
-        boolean xButtonPressed1 = false;
-        
-        //arm
-        boolean xButtonPressed = false;
-
-        boolean aButtonPressed = false;
-        boolean yButtonPressed = false;
-        boolean bButtonPressed = false;
-        double armLiftInput = 0;
-        double armExtendInput = 0;
-        
-        boolean dpadUp2 = false;
-        boolean dpadDown2 = false;
-        boolean dpadLeft2 = false;
-        boolean leftBumperPressed2 = false;
-        boolean rightBumperPressed2 = false;
-        
-        double leftTrigger2 = 0;
-        double rightTrigger2 = 0;
-
         dt.init(hardwareMap);
         cs.init(hardwareMap);
         e1.init(hardwareMap);
@@ -73,71 +41,62 @@ public class StarterBot extends LinearOpMode{
         
         while(opModeIsActive())
         {
-            if (hookMacroActivated)
-            {
-                HookMacro();
-                continue;
-            }
-            targetPowerX = gamepad1.left_stick_x;
-            targetPowerY = -gamepad1.left_stick_y;
-            targetRotation = gamepad1.right_stick_x;
+            double targetPowerX = gamepad1.left_stick_x;
+            double targetPowerY = -gamepad1.left_stick_y;
+            double targetRotation = gamepad1.right_stick_x;
             
-            leftBumperPressed = gamepad1.left_bumper;
-            rightBumperPressed = gamepad1.right_bumper;
-            dpadUp = gamepad1.dpad_up;
-            aButtonPressed1 = gamepad1.a;
-            xButtonPressed1 = gamepad1.x;
+            boolean leftBumperPressed = gamepad1.left_bumper;
+            boolean rightBumperPressed = gamepad1.right_bumper;
+            boolean dpadUp = gamepad1.dpad_up;
+            boolean aButtonPressed1 = gamepad1.a;
+            boolean xButtonPressed1 = gamepad1.x;
             
             //arm
-            xButtonPressed = gamepad2.x;
-
-            aButtonPressed = gamepad2.a;
-            yButtonPressed = gamepad2.y;
-            bButtonPressed = gamepad2.b;
-            armLiftInput = -gamepad2.left_stick_y;
-            armExtendInput = -gamepad2.right_stick_y;
+            boolean xButtonPressed = gamepad2.x;
+            boolean aButtonPressed = gamepad2.a;
+            boolean yButtonPressed = gamepad2.y;
+            double armLiftInput = -gamepad2.left_stick_y;
+            double armExtendInput = -gamepad2.right_stick_y;
             
-            dpadUp2 = gamepad2.dpad_up;
-            dpadDown2 = gamepad2.dpad_down;
-            dpadLeft2 = gamepad2.dpad_left;
-            leftBumperPressed2 = gamepad2.left_bumper;
-            rightBumperPressed2 = gamepad2.right_bumper;
+            boolean dpadUp2 = gamepad2.dpad_up;
+            boolean dpadDown2 = gamepad2.dpad_down;
+            boolean dpadLeft2 = gamepad2.dpad_left;
+            boolean leftBumperPressed2 = gamepad2.left_bumper;
+            boolean rightBumperPressed2 = gamepad2.right_bumper;
             
-            leftTrigger2 = gamepad2.left_trigger;
-            rightTrigger2 = gamepad2.right_trigger;
+            double leftTrigger2 = gamepad2.left_trigger;
+            double rightTrigger2 = gamepad2.right_trigger;
             
-            //claw swapper
-            if (xButtonPressed != lastXButtonState)
+            //claw
+            if (xButtonPressed)
             {
-                cs.clawMove(!cs.GetClawPosition());
+                cs.clawMove(false);
             }
-            lastXButtonState = xButtonPressed;
-            
-            // handles different wrist types based off button presses
-            SwapWristType('D',yButtonPressed);
-            SwapWristType('B',bButtonPressed);
-            SwapWristType('C',aButtonPressed);
-            
-            if (cs.GetWristState() == 'C')
+            else if (aButtonPressed)
             {
-                // 30 degree shifter with triggers
-                if (leftTrigger2 > 0.8 && canShiftCustomWristPos)
+                cs.clawMove(true);
+            }
+            
+            if (yButtonPressed && canShiftWristType)
+            {
+                canShiftWristType = false;
+                if (normalWristType)
                 {
-                    cs.MoveToPosition(cs.GetWristPosition() - 0.1);
-                    canShiftCustomWristPos = false;
-                }
-                else if (rightTrigger2 > 0.8 && canShiftCustomWristPos)
-                {
-                    cs.MoveToPosition(cs.GetWristPosition() + 0.1);
-                    canShiftCustomWristPos = false;
+                    cs.setWristMode('D');
+                    normalWristType = false;
                 }
                 else
                 {
-                    canShiftCustomWristPos = true;
+                    cs.setWristMode('N');
+                    normalWristType = true;
                 }
             }
-
-            cs.Update();
+            else if (!yButtonPressed)
+            {
+                canShiftWristType = true;  
+            }
+            
+            cs.update();
             
             // drive
             if (leftBumperPressed)
@@ -207,33 +166,44 @@ public class StarterBot extends LinearOpMode{
             }
             if (fieldOriented)
             {
-                dt.FieldOrientedTranslate(targetPowerX,targetPowerY,targetRotation, s1.GetImuReading());
+                dt.fieldOrientedTranslate(targetPowerX,targetPowerY,targetRotation, s1.GetImuReading());
             }
             else
             {
-                dt.Translate(targetPowerX,targetPowerY,targetRotation);
+                dt.translate(targetPowerX,targetPowerY,targetRotation);
+            }
+            
+            if (rightTrigger2 > 0.8)
+            {
+                armLiftInput = 1;
+                armExtendInput = 1;
+            }
+            else if (leftTrigger2 > 0.8)
+            {
+                armLiftInput = -1;
+                armExtendInput = -1;
             }
             
             am.rotate(armLiftInput, 'T');
             
             if (armExtendInput > 0.1)
             {
-                e1.Move(armExtendInput,600, 'T');
+                e1.move(armExtendInput,600, 'T');
             }
             else if (armExtendInput < -0.1)
             {
-                e1.Move(armExtendInput,-125,'T');
+                e1.move(armExtendInput,-125,'T');
             }
             else
             {
-                e1.Move(0,e1.GetCurrentPosition(),'T');
+                e1.move(0,e1.GetCurrentPosition(),'T');
             }
             
             telemetry.addData("wrist setting",cs.GetWristState());
             telemetry.addData("wrist position", cs.GetWristPosition());
             telemetry.addData("angle double",am.GetNormalizedArmAngle());
             telemetry.addData("field oriented", fieldOriented);
-            telemetry.Update();
+            telemetry.update();
         }
     }
     
@@ -251,30 +221,4 @@ public class StarterBot extends LinearOpMode{
         canShiftArm = false;
     }
     
-    private void SwapWristType(char additionalMode, boolean buttonPressed)
-    {
-        if (buttonPressed && canShiftWristType)
-        {
-            canShiftWristType = false;
-            if (normalWristType)
-            {
-                cs.setWristMode(additionalMode);
-                normalWristType = false;
-            }
-            else
-            {
-                cs.setWristMode('N');
-                normalWristType = true;
-            }
-        }
-        else if (!buttonPressed && !canShiftWristType)
-        {
-            canShiftWristType = true;  
-        }
-    }
-
-    private void HookMacro()
-    {
-        // add code here
-    }
 }
