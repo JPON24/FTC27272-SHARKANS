b808279cfd8d48ed37897dc6b55ae1eefef51d6e
JPON24 <jpon24@shaw.ca>
2025-01-30 22:59:00 -0700

    Update CommandSequence.java

--- CommandSequence.java
+++ CommandSequence.java
@@ -1,41 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.util.ElapsedTime;
-import java.util.HashMap;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.Drivetrain;
-import org.firstinspires.ftc.teamcode.ClawServo;
-import org.firstinspires.ftc.teamcode.Extension_1;
-import org.firstinspires.ftc.teamcode.ArmLiftMotor;
-import org.firstinspires.ftc.teamcode.OdometrySensor;
-import org.firstinspires.ftc.teamcode.CommandSystem;
+import org.firstinspires.ftc.teamcode.MoveCommand;
 
 @Autonomous
 public class CommandSequence extends LinearOpMode
 {
-    ElapsedTime runtime = new ElapsedTime();
-    
+    MoveCommand moveCmd = new MoveCommand();
     Drivetrain dt = new Drivetrain();
-    ClawServo cs = new ClawServo();
-    ArmLiftMotor am = new ArmLiftMotor();
-    Extension_1 e1 = new Extension_1();
-    OdometrySensor s1 = new OdometrySensor();
-    CommandSystem command = new CommandSystem();
     
     boolean moving = true;
-    double speed = 0.6;
-    int currentAction = 1;
+    double speed = 0.9;
 
     double halfWidth = 7.625;
     double halfLength = 7.5;
-
-    int lastE = 0;
-    int lastA = 0;
-
-
     
     // use later for scalability
     // double halfLength = 8.625; //y
@@ -45,148 +27,13 @@ public class CommandSequence extends LinearOpMode
     {
         moving = true;
     }
-    
-    private void MoveToPosition(double speed, double tgtX, double tgtY, double rot, int tgtE, int tgtA, boolean tgtClaw, char tgtWrist)
-    {
-        // reset for next command
-        command.ResetMap();
-        HashMap<Character, Boolean> localCopy = new HashMap<Character,Boolean>();
-
-        // movement, will be driven by s1.GetBoolsCompleted()
-        command.SetElementFalse('m'); 
-
-        // dont use 0 for minimum position of arm and extension because of this, use 1 instead
-        if (tgtE != lastE) // if extension must occur
-        {
-            command.SetElementFalse('e');
-        }
-        if (tgtA != lastA) // if arm movement must occur
-        {
-            command.SetElementFalse('a');
-        }
-        if (tgtClaw != cs.GetClawPosition()) // if move has to happen
-        {
-            command.SetElementFalse('c');
-        }
-        if (tgtWrist != cs.GetWristState()) // if move has to happen
-        {
-            command.SetElementFalse('w');
-        }
-        lastE = tgtE;
-        lastA = tgtA;
-        
-        s1.OdometryControl(speed,tgtX,tgtY,rot);
-        localCopy = command.GetMap();
-        
-        while (!command.GetBoolsCompleted())
-        {
-            // for every key (m, e, a, c, w)
-            for (Character key : localCopy.keySet())
-            {
-                // like an if else but more efficient
-                // if a subsystem has reached it's position, set it to complete
-                // otherwise, set it to false and stop moving it 
-                switch (key)
-                {
-                    case 'm':
-                        if (s1.GetBoolsCompleted())
-                        {
-                            command.SetElementTrue('m');
-                            break;
-                        }
-                        else
-                        {
-                            command.SetElementFalse('m');
-                            s1.OdometryControl(speed,tgtX,tgtY,rot);
-                            break;
-                        }
-                    case 'e':
-                        if (e1.GetCompleted(tgtE))
-                        {
-                            command.SetElementTrue('e');
-                            break;
-                        }
-                        else
-                        {
-                            command.SetElementFalse('e');
-                            e1.move(speed,tgtE,'A');
-                            break;
-                        }
-                    case 'a':
-                        if (am.GetCompleted(tgtA))
-                        {
-                            command.SetElementTrue('a');
-                            break;
-                        }
-                        else
-                        {
-                            command.SetElementFalse('a');
-                            am.rotate(tgtA,'A');
-                            break;
-                        }
-                    case 'c':
-                        if (cs.GetClawPosition() == tgtClaw)
-                        {
-                            command.SetElementTrue('c');
-                            break;
-                        }
-                        else
-                        {
-                            command.SetElementFalse('c');
-                            cs.clawMove(tgtClaw);
-                            break;
-                        }
-                    case 'w':
-                        if (cs.GetWristState() == tgtWrist)
-                        {
-                            command.SetElementTrue('w');
-                            break;
-                        }
-                        else
-                        {
-                            command.SetElementFalse('w');
-                            cs.setWristMode(tgtWrist);
-                            break;
-                        }
-                }
-            }
-            
-            telemetry.addData("current action",currentAction);
-            telemetry.addData("volts normalized",hardwareMap.getAll(VoltageSensor.class).get(0).getVoltage());
-            telemetry.addData("speed",speed);
-            telemetry.addData("x",s1.GetPositionX());
-            telemetry.addData("y",s1.GetPositionY());
-            telemetry.addData("h",s1.GetImuReading());
-            telemetry.addData("error x",s1.GetErrorX());
-            telemetry.addData("error y",s1.GetErrorY());
-            telemetry.addData("error h",s1.GetErrorH());
-            telemetry.addData("position a",am.GetCurrentPosition());
-            telemetry.addData("position e",e1.extension_1.getCurrentPosition());
-            telemetry.addData("position claw",cs.GetClawPosition());
-            telemetry.addData("position wrist",cs.GetWristState());
-            telemetry.update();
-            UpdateClaw();
-        }
-        runtime.reset();
-        currentAction += 1;
-    }
 
     private void Stop()
     {
-        dt.fieldOrientedTranslate(0,0,0,s1.GetImuReading());
+        dt.FieldOrientedTranslate(0,0,0,0);
         moving = false;
     }
     
-    private void UpdateClaw()
-    {
-        cs.update();
-    }
-    
-    
-    /*
-     * will require updating - adding the extra parameters for other movements
-     * refer to method for determining what values to add
-     */
     private void CommandSequence()
     {
         /* Parameter order --
@@ -195,95 +42,103 @@ public class CommandSequence extends LinearOpMode
         double y position : range > 0
         double rotation : range 0 - 360
         int tgtExtension : range 0 - 800
-        int tgtArmRotation : range -15, 5, 30, 45, 50, 60, 70, 80, 90
+        int tgtArmRotation : range 0-260
         boolean tgtClawOpen
-        char wristPosition : range 'N', 'D'
+        char wristPosition : range 'N', 'D', 'B', 'C'
         */
         
         // length = 15 inches
         // width = 15.25 inches
         // height = 15 inches
-        // 211 degree ROM
-
-        // first hook
-        MoveToPosition(speed,0,46 - 2 * halfLength,0,0,0,false,'D');
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,true,'N');
+        // 260 degree ROM
         
-        // // grab second specimen off wall
-        // MoveToPosition(speed,48,15,0,0,211,true,'N');
-        // MoveToPosition(speed,48,15,0,0,211,false,'N');
+        // hook
+        Hook(0);
+        
+        // push
+        moveCmd.MoveToPosition(speed,-26,-19,0,0,80,false,'N'); //4
+        moveCmd.MoveToPosition(speed,-26,-48,0,0,60,false,'N'); //5
+        moveCmd.MoveToPosition(speed,-37,-48,0,0,50,false,'N'); //6
+        moveCmd.MoveToPosition(speed,-37,-9,0,0,30,false,'N'); //7
 
-        // // second hook
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,76,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,true,'N');
+        // grab
+        moveCmd.MoveToPosition(speed,-37,-9,0,0,30,false,'N'); //8
+        moveCmd.MoveToPosition(speed,-37,-9,0,0,30,true,'N'); //8
+        moveCmd.MoveToPosition(speed,-37,-9,0,0,60,true,'N');
+        
+        // hook
+        Hook(2);
+        
+        // push
+        moveCmd.MoveToPosition(speed,-36,-19,0,0,60,false,'N'); //11
+        moveCmd.MoveToPosition(speed,-36,-48,0,0,60,true,'N'); //11
+        moveCmd.MoveToPosition(speed,-47,-48,0,0,50,true,'N'); //12
+        moveCmd.MoveToPosition(speed,-47,-9,0,0,30,true,'N'); //13
+        
+        // grab
+        moveCmd.MoveToPosition(speed,-47,-9,0,0,30,false,'N'); //14
+        moveCmd.MoveToPosition(speed,-47,-9,0,0,30,true,'N'); //14
+        moveCmd.MoveToPosition(speed,-47,-9,0,0,60,true,'N');
+    
+        // hook
+        Hook(4);
 
-        // // grab next sample
-        // MoveToPosition(speed,30,42 - halfLength,0,0,211,true,'N');
-        // MoveToPosition(speed,48 - halfWidth,48,0,0,211,true,'N');
+        // push
+        moveCmd.MoveToPosition(speed,-46,-19,0,0,60,false,'N'); //11
+        moveCmd.MoveToPosition(speed,-46,-48,0,0,40,true,'N'); //17
+        moveCmd.MoveToPosition(speed,-52,-48,0,0,30,true,'N'); //18
+        moveCmd.MoveToPosition(speed,-52,-9,0,0,30,true,'N'); //19
 
-        // // wait and then regrab specimen
-        // MoveToPosition(speed,48 - halfWidth,15,0,0,111,true,'N');
-        // sleep(500);
-        // MoveToPosition(speed,48 - halfWidth,15,0,0,211,true,'N');
-        // MoveToPosition(speed,48 - halfWidth,15,0,0,211,false,'N');
+        // grab
+        moveCmd.MoveToPosition(speed,-52,-9,0,0,30,false,'N'); //20
+        moveCmd.MoveToPosition(speed,-52,-9,0,0,30,true,'N'); //20
+        moveCmd.MoveToPosition(speed,-52,-9,0,0,60,true,'N');
 
-        // // third hook
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,76,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,true,'N');
+        // hook
+        Hook(6);
 
-        // // grab next sample
-        // MoveToPosition(speed,30,42 - halfLength,0,0,211,true,'N');
-        // MoveToPosition(speed,58 - halfWidth,48,0,0,211,true,'N');
+        //grab
+        Grab();
 
-        // // wait and then regrab specimen
-        // MoveToPosition(speed,58 - halfWidth,15,0,0,111,true,'N');
-        // sleep(500);
-        // MoveToPosition(speed,58 - halfWidth,15,0,0,211,true,'N');
-        // MoveToPosition(speed,58 - halfWidth,15,0,0,211,false,'N');
-
-        // // fourth hook
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,76,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,true,'N');
-
-        // // grab next sample
-        // MoveToPosition(speed,30,42 - halfLength,0,0,211,true,'N');
-        // MoveToPosition(speed,68 - halfWidth,48,0,0,211,true,'N');
-
-        // // wait and then regrab specimen
-        // MoveToPosition(speed,68 - halfWidth,15,0,0,111,true,'N');
-        // sleep(500);
-        // MoveToPosition(speed,68 - halfWidth,15,0,0,211,true,'N');
-        // MoveToPosition(speed,68 - halfWidth,15,0,0,211,false,'N');
-
-        // // fifth hook
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,76,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,false,'N'); 
-        // MoveToPosition(speed,0,48 - 2 * halfLength,0,0,70,true,'N');
+        // hook
+        Hook(8);
 
         // end sequence
+        moveCmd.MoveToPosition(speed,-35,-4,0,0,0,false,'N');
         Stop();
     }
-    
+
+    private void Hook(double hookOffset) // add offset constant
+    {
+        // arm speed turned down to prevent tipping
+        moveCmd.MoveToPosition(speed,0 - hookOffset,-6,0,0,120,true,'D'); //1
+        moveCmd.MoveToPosition(speed,0 - hookOffset,-12,0,0,145,true,'D'); //1
+        moveCmd.MoveToPosition(speed,0 - hookOffset,-23,0,0,145,true,'B'); //1
+        moveCmd.MoveToPosition(speed,0 - hookOffset,-23,0,0,165,true,'B'); //1
+        moveCmd.MoveToPosition(speed,0 - hookOffset,-21,0,0,165,false,'B'); //1
+    }
+
+    private void Grab()
+    {
+        moveCmd.MoveToPosition(speed,-27.5,-9,0,0,30,false,'N'); //23
+        moveCmd.MoveToPosition(speed,-27.5,-9,0,0,30,true,'N'); //23
+        moveCmd.MoveToPosition(speed,-27.5,-9,0,0,60,true,'N');
+    }
+
     private void TestSequence()
     {
-        MoveToPosition(speed,12,12,90,0,0,false,'N');
+        moveCmd.MoveToPosition(speed,0,0,0,0,165,true,'N');
+        // moveCmd.MoveToPosition(speed,0,0,0,0,40,false,'N');
     }
     
     @Override
     public void runOpMode() 
     {
         dt.init(hardwareMap);
-        cs.init(hardwareMap);
-        am.init(hardwareMap);
-        e1.init(hardwareMap);
-        s1.init(hardwareMap);
+        moveCmd.init(hardwareMap, true);
         
         double volts = hardwareMap.getAll(VoltageSensor.class).get(0).getVoltage();
-        double normalizedVolts = (1 - volts/14) + 0.7586;
+        double normalizedVolts = (1 - volts/14) + 0.57; // reduced to account for volt drops during auton
         speed *= normalizedVolts;
         
         waitForStart();
@@ -292,10 +147,11 @@ public class CommandSequence extends LinearOpMode
         {
             if (moving)
             {
+                // maybe add arm speed change when hover/moving
+                
                 CommandSequence();
-                //TestSequence();
+                // TestSequence();
             }
         }
     }
 }
-
