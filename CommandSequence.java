b57a4bac8c7232ea5d28b876580fc9f4c00f6cb7
Elizabeth6432 <edythe952@stemia.ca>
2025-01-31 20:46:03 -0700

    Update CommandSequence.java

--- CommandSequence.java
+++ CommandSequence.java
@@ -14,7 +14,7 @@ public class CommandSequence extends LinearOpMode
     Drivetrain dt = new Drivetrain();
     
     boolean moving = true;
-    double speed = 0.9;
+    double speed = 0.5;
 
     double halfWidth = 7.625;
     double halfLength = 7.5;
@@ -42,9 +42,9 @@ public class CommandSequence extends LinearOpMode
         double y position : range > 0
         double rotation : range 0 - 360
         int tgtExtension : range 0 - 800
-        int tgtArmRotation : range 0-260
+        int tgtArmRotation : range -15, 5, 30, 45, 50, 60, 70, 80, 90
         boolean tgtClawOpen
-        char wristPosition : range 'N', 'D', 'B', 'C'
+        char wristPosition : range 'N', 'D', 'B'
         */
         
         // length = 15 inches
@@ -53,82 +53,79 @@ public class CommandSequence extends LinearOpMode
         // 260 degree ROM
         
         // hook
-        Hook(0);
+        Hook();
         
         // push
-        moveCmd.MoveToPosition(speed,-26,-19,0,0,80,false,'N'); //4
+        moveCmd.MoveToPosition(speed,-26,-19,0,0,110,false,'N'); //4
         moveCmd.MoveToPosition(speed,-26,-48,0,0,60,false,'N'); //5
         moveCmd.MoveToPosition(speed,-37,-48,0,0,50,false,'N'); //6
-        moveCmd.MoveToPosition(speed,-37,-9,0,0,30,false,'N'); //7
+        moveCmd.MoveToPosition(speed,-37,-8,0,0,30,false,'N'); //7
 
         // grab
-        moveCmd.MoveToPosition(speed,-37,-9,0,0,30,false,'N'); //8
-        moveCmd.MoveToPosition(speed,-37,-9,0,0,30,true,'N'); //8
-        moveCmd.MoveToPosition(speed,-37,-9,0,0,60,true,'N');
+        moveCmd.MoveToPosition(speed,-37,-8,0,0,30,true,'N'); //8
         
         // hook
-        Hook(2);
+        Hook();
         
         // push
-        moveCmd.MoveToPosition(speed,-36,-19,0,0,60,false,'N'); //11
+        moveCmd.MoveToPosition(speed,-36,-19,0,0,110,false,'N'); //11
         moveCmd.MoveToPosition(speed,-36,-48,0,0,60,true,'N'); //11
         moveCmd.MoveToPosition(speed,-47,-48,0,0,50,true,'N'); //12
-        moveCmd.MoveToPosition(speed,-47,-9,0,0,30,true,'N'); //13
+        moveCmd.MoveToPosition(speed,-47,-9,0,0,30,false,'N'); //13
         
         // grab
-        moveCmd.MoveToPosition(speed,-47,-9,0,0,30,false,'N'); //14
-        moveCmd.MoveToPosition(speed,-47,-9,0,0,30,true,'N'); //14
         moveCmd.MoveToPosition(speed,-47,-9,0,0,60,true,'N');
     
         // hook
-        Hook(4);
+        Hook();
 
         // push
         moveCmd.MoveToPosition(speed,-46,-19,0,0,60,false,'N'); //11
         moveCmd.MoveToPosition(speed,-46,-48,0,0,40,true,'N'); //17
         moveCmd.MoveToPosition(speed,-52,-48,0,0,30,true,'N'); //18
-        moveCmd.MoveToPosition(speed,-52,-9,0,0,30,true,'N'); //19
+        moveCmd.MoveToPosition(speed,-52,-8,0,0,30,true,'N'); //19
 
         // grab
-        moveCmd.MoveToPosition(speed,-52,-9,0,0,30,false,'N'); //20
-        moveCmd.MoveToPosition(speed,-52,-9,0,0,30,true,'N'); //20
-        moveCmd.MoveToPosition(speed,-52,-9,0,0,60,true,'N');
+        moveCmd.MoveToPosition(speed,-52,-8,0,0,30,false,'N'); //20
+        moveCmd.MoveToPosition(speed,-52,-8,0,0,30,true,'N'); //20
 
         // hook
-        Hook(6);
+        Hook();
 
         //grab
         Grab();
 
         // hook
-        Hook(8);
+        Hook();
 
         // end sequence
         moveCmd.MoveToPosition(speed,-35,-4,0,0,0,false,'N');
         Stop();
     }
 
-    private void Hook(double hookOffset) // add offset constant
+    private void Hook() // add offset constant
     {
-        // arm speed turned down to prevent tipping
-        moveCmd.MoveToPosition(speed,0 - hookOffset,-6,0,0,120,true,'D'); //1
-        moveCmd.MoveToPosition(speed,0 - hookOffset,-12,0,0,145,true,'D'); //1
-        moveCmd.MoveToPosition(speed,0 - hookOffset,-23,0,0,145,true,'B'); //1
-        moveCmd.MoveToPosition(speed,0 - hookOffset,-23,0,0,165,true,'B'); //1
-        moveCmd.MoveToPosition(speed,0 - hookOffset,-21,0,0,165,false,'B'); //1
+        // deprecated
+        // moveCmd.MoveToPosition(speed,0,-12,0,0,150,true,'D'); //1
+        // moveCmd.MoveToPosition(speed,0,-24,0,0,150,true,'D'); //1
+        // moveCmd.MoveToPosition(speed,0,-24,0,0,150,true,'B'); //1
+        // moveCmd.MoveToPosition(speed,0,-21.5,0,0,150,true,'B'); //1
+        moveCmd.MoveToPosition(speed,0,-2,0,0,0,true,'D'); //1
+        moveCmd.MoveToPosition(speed,0,-6,0,0,130,true,'D');
+        moveCmd.MoveToPosition(speed,0,-18,0,0,130,true,'D'); //1
+        moveCmd.MoveToPosition(speed,0,-18,0,650,130,true,'D'); //1
     }
 
     private void Grab()
     {
-        moveCmd.MoveToPosition(speed,-27.5,-9,0,0,30,false,'N'); //23
-        moveCmd.MoveToPosition(speed,-27.5,-9,0,0,30,true,'N'); //23
-        moveCmd.MoveToPosition(speed,-27.5,-9,0,0,60,true,'N');
+        moveCmd.MoveToPosition(speed,-27.5,-8,0,0,30,false,'N'); //23
+        moveCmd.MoveToPosition(speed,-27.5,-8,0,0,30,true,'N'); //23
     }
 
     private void TestSequence()
     {
-        moveCmd.MoveToPosition(speed,0,0,0,0,165,true,'N');
-        // moveCmd.MoveToPosition(speed,0,0,0,0,40,false,'N');
+        moveCmd.MoveToPosition(speed,0,0,0,0,0,true,'N');
+        // moveCmd.MoveToPosition(speed,0,-16,0,0,40,false,'N');
     }
     
     @Override
