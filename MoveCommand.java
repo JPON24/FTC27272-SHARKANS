c74c593a0923e40bd909a0ad9b9fba69be206fe9
JPON24 <jpon24@shaw.ca>
2025-02-23 23:13:32 -0700

    Update MoveCommand.java

--- MoveCommand.java
+++ MoveCommand.java
@@ -7,7 +7,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.Drivetrain;
 import org.firstinspires.ftc.teamcode.ClawServo;
-import org.firstinspires.ftc.teamcode.Extension_1;
 import org.firstinspires.ftc.teamcode.ArmLiftMotor;
 import org.firstinspires.ftc.teamcode.OdometrySensor;
 import org.firstinspires.ftc.teamcode.CommandSystem;
@@ -16,11 +15,9 @@ public class MoveCommand  {
     Drivetrain dt = new Drivetrain();
     ClawServo cs = new ClawServo();
     ArmLiftMotor am = new ArmLiftMotor();
-    Extension_1 e1 = new Extension_1();
     OdometrySensor s1 = new OdometrySensor();
     CommandSystem command = new CommandSystem();
 
-    int lastE = 0;
     int lastA = 0;
     
     public void init(HardwareMap hwMap, boolean isAuton)
@@ -28,7 +25,6 @@ public class MoveCommand  {
         dt.init(hwMap);
         cs.init(hwMap, true);
         am.init(hwMap, true);
-        e1.init(hwMap);
         s1.init(hwMap, isAuton);
     }
 
@@ -46,17 +42,16 @@ public class MoveCommand  {
             command.SetElementFalse('a');
         }
 
-        cs.SetWristMode(tgtWrist);
-
 //        command.SetElementFalse('c');
-        lastE = tgtE;
         lastA = tgtA;
         
         s1.OdometryControl(speed,tgtX,tgtY,rot);
+
         localCopy = command.GetMap();
         
         while (!command.GetBoolsCompleted())
         {
+            cs.SetWristMode(tgtWrist);
             // for every key (m, e, a, c, w)
             for (Character key : localCopy.keySet())
             {
@@ -103,60 +98,52 @@ public class MoveCommand  {
 //                            break;
 //                        }
                 }
-                cs.Update();
             }
+
+            cs.Update();
         }
     }
 
-    public void MoveToPositionCancellable(int tgtE, int tgtA, boolean tgtClaw, char tgtWrist)
+    public void MoveToPositionCancellable(double speed, double x, double y, double h, int tgtA, boolean tgtClaw, char tgtWrist)
     {
         // reset for next command
         command.ResetMap();
         HashMap<Character, Boolean> localCopy = new HashMap<Character,Boolean>();
 
-        // dont use 0 for minimum position of arm and extension because of this, use 1 instead
-        if (tgtE != lastE) // if extension must occur
-        {
-            command.SetElementFalse('e');
-        }
-        if (tgtA != lastA) // if arm movement must occur
-        {
-            command.SetElementFalse('a');
-        }
-        if (tgtClaw != cs.GetClawClosed()) // if move has to happen
-        {
-            command.SetElementFalse('c');
-        }
-        if (tgtWrist != cs.GetWristState()) // if move has to happen
-        {
-            command.SetElementFalse('w');
-        }
-        lastE = tgtE;
-        lastA = tgtA;
+        command.SetElementFalse('m');
+        command.SetElementFalse('a');
+
+//        if (tgtClaw != cs.GetClawClosed()) // if move has to happen
+//        {
+//            command.SetElementFalse('c');
+//        }
 
         localCopy = command.GetMap();
 
         // for every key (m, e, a, c, w)
         for (Character key : localCopy.keySet())
         {
+            cs.SetWristMode(tgtWrist);
             // like an if else but more efficient
             // if a subsystem has reached it's position, set it to complete
             // otherwise, set it to false and stop moving it 
             switch (key)
             {
-                case 'e':
-                    if (e1.GetCompleted(tgtE))
+                case 'm':
+                    s1.OdometryControl(speed,x,y,h);
+                    if (s1.GetBoolsCompleted())
                     {
-                        command.SetElementTrue('e');
+                        command.SetElementTrue('m');
+                        // dt.FieldOrientedTranslate(0,0,0,0);
                         break;
                     }
                     else
                     {
-                        command.SetElementFalse('e');
-                        e1.Move(1,tgtE,'A');
+                        command.SetElementFalse('m');
                         break;
                     }
                 case 'a':
+                    am.rotate(tgtA,'A');
                     if (am.GetCompleted(tgtA))
                     {
                         command.SetElementTrue('a');
@@ -165,33 +152,20 @@ public class MoveCommand  {
                     else
                     {
                         command.SetElementFalse('a');
-                        am.rotate(tgtA,'A');
                         break;
                     }
-                case 'c':
-                    if (cs.GetClawClosed() == tgtClaw)
-                    {
-                        command.SetElementTrue('c');
-                        break;
-                    }
-                    else
-                    {
-                        command.SetElementFalse('c');
-                        cs.clawMove(tgtClaw);
-                        break;
-                    }
-                case 'w':
-                    if (cs.GetWristState() == tgtWrist)
-                    {
-                        command.SetElementTrue('w');
-                        break;
-                    }
-                    else
-                    {
-                        command.SetElementFalse('w');
-                        cs.SetWristMode(tgtWrist);
-                        break;
-                    }
+//                case 'c':
+//                    if (cs.GetClawClosed() == tgtClaw)
+//                    {
+//                        command.SetElementTrue('c');
+//                        break;
+//                    }
+//                    else
+//                    {
+//                        command.SetElementFalse('c');
+//                        cs.clawMove(tgtClaw);
+//                        break;
+//                    }
             }
         }
         cs.Update();
