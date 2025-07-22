9e5053a7600e4b17d35bdf3df4477d5568263198
JPON24 <jpon24@shaw.ca>
2025-07-22 15:00:00 -0600

    added for pid constants (realtime tuner)

---
+++ SampleConstants.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/*
+ * Some additional constants for testing.
+ */
+//@Config
+public class SampleConstants {
+//    public static DcMotorSimple.Direction DIR = DcMotorSimple.Direction.FORWARD;
+//    public static boolean FF_ENABLED = false;
+
+    public static double KPX = 0.36;
+    public static double KPY = 0.3;
+    public static double KDX = 0.2;
+    public static double KDY = 0.2;
+
+    /*
+    kpx = 0.16
+    kpy = 0.16
+    kdx = 0.14
+    kdy = 0.14
+     */
+}
